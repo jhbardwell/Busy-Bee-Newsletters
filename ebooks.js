52796b5afd6c0ebc0b9431091304a69ef2bda449
jhbardwell <61172970+jhbardwell@users.noreply.github.com>
2022-03-02 19:12:10 -0500

    Update ebooks.js

--- ebooks.js
+++ ebooks.js
@@ -1,91 +1,91 @@
-{
-  const ebookData =  [
-    {
-      id: "Arenson_2018_01",
-      price: 0.99,
-      title: "Requiem",
-      series: "The Dragonfire Rain Series",
-      blurb: "Children in the Kingdom of Requiem are born human. Once mature, they grow wings and breathe fire, reborn as dragons. For eons, Nemoria the Devourer has slept. Now, she too is reborn, and this dark goddess craves the hot magic blood of dragons. No fire can burn her. No claws can pierce her. Countless dragons attack. All of them die. Only the chosen one can stop her cruel rampage.",
-      author: {
-        name: "Arenson, D.",
-        firstname: "Daniel",
-        lastname: "Arenson"
-      },
-      date: {
-        puiblication: "11/18/2018",
-        lastnlcampaign: "",
-        scheduledfofcampaign: "",
-        lastfofcampaign: "10/05/2021"
-      },
-      stats: {
-        printpages: 274,
-        amazonratingscore: 4.6,
-        amazonratings: 61
-      },
-      url: {
-        amazon: "",
-        sales: "https://books2read.com/u/bWRvRz",
-        cover: "https://books2read-prod.s3.amazonaws.com/CACHE/images/ubl_book_images/bWRvRz/ea4a31e5820230fbfcf1eecd131f058a"
-      },
-      keywords: [ "dark", "dragons", "fantasy", "humor", "shifters", "swords and sorcery" ]
+
+const ebookData =  [
+  {
+    id: "Arenson_2018_01",
+    price: 0.99,
+    title: "Requiem",
+    series: "The Dragonfire Rain Series",
+    blurb: "Children in the Kingdom of Requiem are born human. Once mature, they grow wings and breathe fire, reborn as dragons. For eons, Nemoria the Devourer has slept. Now, she too is reborn, and this dark goddess craves the hot magic blood of dragons. No fire can burn her. No claws can pierce her. Countless dragons attack. All of them die. Only the chosen one can stop her cruel rampage.",
+    author: {
+      name: "Arenson, D.",
+      firstname: "Daniel",
+      lastname: "Arenson"
     },
-    {
-      id: "Ashwood_2017_01",
-      price: 0.00,
-      title: "The Seekers",
-      series: "The Order of the Lily Series",
-      blurb: "Life without her husband had lost its flare for Audrey. Then a handsome stranger saves her from a violent attack, reigniting a spark she thought long extinguished. But his interest is dark and unsavory. He wants more than mere hot blooded passion. He wants the power coursing through her veins, a treacherous gift once lost to the ashes of time.",
-      author: {
-        name: "Ashwood, C.",
-        firstname: "Cait",
-        lastname: "Ashwood"
-      },
-      date: {
-        puiblication: "01/21/2017",
-        lastnlcampaign: "",
-        scheduledfofcampaign: "",
-        lastfofcampaign: "10/05/2021"
-      },
-      stats: {
-        printpages: 428,
-        amazonratingscore: 4.3,
-        amazonratings: 161
-      },
-      url: {
-        amazon: "",
-        sales: "https://books2read.com/u/4A5GJo",
-        cover: "https://books2read-prod.s3.amazonaws.com/CACHE/images/ubl_book_images/4A5GJo/36adad02fdf90dc5af74045d18d15cce"
-      },
-      keywords: [ "dark", "epic", "dystopian", "fantasy", "romance", "time travel" ]
+    date: {
+      puiblication: "11/18/2018",
+      lastnlcampaign: "",
+      scheduledfofcampaign: "",
+      lastfofcampaign: "10/05/2021"
     },
-    {
-      id: "Bardwell_2017_01",
-      price: 0.00,
-      title: "Rotten Magic",
-      series: "The Artifice Mage Saga",
-      blurb: "Artificers are gilded princes of the Iron Empire. Mages are insane criminals. Devin competes bone to the brass with his friend Drusilla to become the best artificer in the empire. But this power hungry young apprentice hides a dangerous secret.",
-      author: {
-        name: "Bardwell, J.",
-        firstname: "Jeffrey",
-        lastname: "Bardwell"
-      },
-      date: {
-        puiblication: "08/09/2017",
-        lastnlcampaign: "",
-        scheduledfofcampaign: "",
-        lastfofcampaign: "11/15/2021"
-      },
-      stats: {
-        printpages: 416,
-        amazonratingscore: 3.9,
-        amazonratings: 82
-      },
-      url: {
-        amazon: "",
-        sales: "https://books2read.com/rotten-magic",
-        cover: "https://books2read-prod.s3.amazonaws.com/CACHE/images/ubl_book_images/display_image_KmSwU66/10e652efee186eefd0e731887160ad81"
-      },
-      keywords: [ "coming of age", "dark", "epic", "fantasy", "steampunk", "young adult" ]
-    }
-  ]
-}
+    stats: {
+      printpages: 274,
+      amazonratingscore: 4.6,
+      amazonratings: 61
+    },
+    url: {
+      amazon: "",
+      sales: "https://books2read.com/u/bWRvRz",
+      cover: "https://books2read-prod.s3.amazonaws.com/CACHE/images/ubl_book_images/bWRvRz/ea4a31e5820230fbfcf1eecd131f058a"
+    },
+    keywords: [ "dark", "dragons", "fantasy", "humor", "shifters", "swords and sorcery" ]
+  },
+  {
+    id: "Ashwood_2017_01",
+    price: 0.00,
+    title: "The Seekers",
+    series: "The Order of the Lily Series",
+    blurb: "Life without her husband had lost its flare for Audrey. Then a handsome stranger saves her from a violent attack, reigniting a spark she thought long extinguished. But his interest is dark and unsavory. He wants more than mere hot blooded passion. He wants the power coursing through her veins, a treacherous gift once lost to the ashes of time.",
+    author: {
+      name: "Ashwood, C.",
+      firstname: "Cait",
+      lastname: "Ashwood"
+    },
+    date: {
+      puiblication: "01/21/2017",
+      lastnlcampaign: "",
+      scheduledfofcampaign: "",
+      lastfofcampaign: "10/05/2021"
+    },
+    stats: {
+      printpages: 428,
+      amazonratingscore: 4.3,
+      amazonratings: 161
+    },
+    url: {
+      amazon: "",
+      sales: "https://books2read.com/u/4A5GJo",
+      cover: "https://books2read-prod.s3.amazonaws.com/CACHE/images/ubl_book_images/4A5GJo/36adad02fdf90dc5af74045d18d15cce"
+    },
+    keywords: [ "dark", "epic", "dystopian", "fantasy", "romance", "time travel" ]
+  },
+  {
+    id: "Bardwell_2017_01",
+    price: 0.00,
+    title: "Rotten Magic",
+    series: "The Artifice Mage Saga",
+    blurb: "Artificers are gilded princes of the Iron Empire. Mages are insane criminals. Devin competes bone to the brass with his friend Drusilla to become the best artificer in the empire. But this power hungry young apprentice hides a dangerous secret.",
+    author: {
+      name: "Bardwell, J.",
+      firstname: "Jeffrey",
+      lastname: "Bardwell"
+    },
+    date: {
+      puiblication: "08/09/2017",
+      lastnlcampaign: "",
+      scheduledfofcampaign: "",
+      lastfofcampaign: "11/15/2021"
+    },
+    stats: {
+      printpages: 416,
+      amazonratingscore: 3.9,
+      amazonratings: 82
+    },
+    url: {
+      amazon: "",
+      sales: "https://books2read.com/rotten-magic",
+      cover: "https://books2read-prod.s3.amazonaws.com/CACHE/images/ubl_book_images/display_image_KmSwU66/10e652efee186eefd0e731887160ad81"
+    },
+    keywords: [ "coming of age", "dark", "epic", "fantasy", "steampunk", "young adult" ]
+  }
+];
+
