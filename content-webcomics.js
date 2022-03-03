dfe1dd01fa1a468a06b6f8bad668cec1f2f7b8c6
jhbardwell <61172970+jhbardwell@users.noreply.github.com>
2022-03-02 21:11:46 -0500

    Add files via upload

---
+++ content-webcomics.js
@@ -0,0 +1,36 @@
+{
+  const webcomicData =  [
+    {
+      id: "Dahm_2010_01",
+      price: 0.00,
+      title: "Vattu",
+      review: "This is but one heroic tale among an entire world of intriguing tales: a coming-of-age story featuring the young barbarian, Vattu, who is enslaved and unceremoniously pitched head first into a quasi-Roman civilization. Yes, it's a story we've heard before, but it has enough unique twists to entertain. The art is simple, but stylized, the characters are human (at least in the range of their emotions), and the plot puts a warm face on the 'evil' jingoistic, quasi-Roman imperial dogs. They are literally dogs. Even without canid Romans, I would love this webcomic for the rich, colorful universe the author has woven with words and ink. This is but a corner of a much larger tapestry. Check it out!",
+      author: {
+        name: "Dahm, E.",
+        firstname: "Evan",
+        lastname: "Dahm"
+      },
+      date: {
+        lastnlcampaign: "01/02/2018"
+      },
+      url: "http://www.rice-boy.com/vattu/",
+      keywords: [ "adventure", "fantasy" ]
+    },
+    {
+      id: "Sundberg_2013_01",
+      price: 0.00,
+      title: "Stand Still. Stay Silent.",
+      review: "A haunting glimpse of a dystopia where monsters roam the earth and the only havens for humanity are isolated scandanavian enclaves. While the rest of the world is empty and devoid of life. Engaging art. Great characters, lore, and languages. Check it out!",
+      author: {
+        name: "Sundberg, M.",
+        firstname: "Minna",
+        lastname: "Sundberg"
+      },
+      date: {
+        lastnlcampaign: "10/05/2021"
+      },
+      url: "http://www.sssscomic.com/",
+      keywords: [ "dystopian", "monsters", "norse myh", "post apocalyptic", "survival horror" ]
+    }
+  ]
+}
